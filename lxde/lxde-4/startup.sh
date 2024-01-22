ac09fd1814452d7bede4186e651b181a90825d28
mint-jetos <mint.jetos@gmail.com>
2024-01-22 22:09:01 +0000

    lxde commit

---
+++ lxde/lxde-4/startup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -n "$VNC_PASSWORD" ]; then
+    echo -n "$VNC_PASSWORD" > /.password1
+    x11vnc -storepasswd $(cat /.password1) /.password2
+    chmod 400 /.password*
+    sed -i 's/^command=x11vnc.*/& -rfbauth \/.password2/' /etc/supervisor/conf.d/supervisord.conf
+    export VNC_PASSWORD=
+fi
+
+if [ -n "$RESOLUTION" ]; then
+    sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
+fi
+
+USER=${USER:-root}
+if [ "$USER" != "root" ]; then
+    echo "* enable custom user: $USER"
+    useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+    if [ -z "$PASSWORD" ]; then
+        echo "  set default password to \"ubuntu\""
+        PASSWORD=ubuntu
+    fi
+    HOME=/home/$USER
+    echo "$USER:$PASSWORD" | chpasswd
+    # cp -r /root/{.config,.gtkrc-2.0,.asoundrc} ${HOME}
+    chown -R $USER:$USER ${HOME}
+    #[ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+fi
+
+exec  /usr/bin/supervisord  -n -c /etc/supervisor/supervisord.conf
